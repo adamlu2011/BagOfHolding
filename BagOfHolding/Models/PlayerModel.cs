dfa47468eb0ad47c86163f7cb6cd496c195cf8ee
adamlu2011 <73263799+adamlu2011@users.noreply.github.com>
2026-03-10 16:04:59 -0500

    Added player model and models folder

---
+++ BagOfHolding/Models/PlayerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagOfHolding.Models
+{
+    public class PlayerModel
+    {
+        [Required]
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; } = string.Empty;
+        public string CharacterName { get; set; } = string.Empty;
+    }
+}
